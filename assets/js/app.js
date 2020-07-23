c3c0844e8d906489ffcccccb9815c61292c58265
zubayerhimel <zhimel007@gmail.com>
2020-07-23 18:44:57 +0600

    added project section

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,6 +2,13 @@ $(function () {
   $(document).on("click", "ul li", function () {
     $(this).addClass("active").siblings().removeClass("active");
   });
+
+  $(window).on("load", function () {
+    $(".skill_row_percent_view").each(function () {
+      thisVal = $(this).text();
+      $(this).parent(".skill_row_percent").animate({ width: thisVal }, 200);
+    });
+  });
 });
 
 document.addEventListener("DOMContentLoaded", function () {
