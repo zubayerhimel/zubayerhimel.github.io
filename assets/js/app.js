9e2832c589ae242c1691dfbc2a991e56acee25c1
zubayerhimel <zhimel007@gmail.com>
2020-08-03 10:18:17 +0600

    added functionality in contact form

--- assets/js/app.js
+++ assets/js/app.js
@@ -18,17 +18,37 @@ $(function () {
 
   // skill bar color set
   $(window).on("load", function () {
+    $(".progress").hide();
+    $("#main-content").fadeIn(100);
     $(".skill_row_percent_view").each(function () {
       thisVal = $(this).text();
       $(this).parent(".skill_row_percent").animate({ width: thisVal }, 200);
     });
   });
-});
 
-// banner svg animate
-document.addEventListener("DOMContentLoaded", function () {
-  window.setTimeout(
-    document.querySelector("#bannersvg").classList.add("animated"),
-    1000
-  );
+  // Contact form functionality
+  $("#contact-form").submit(function (e) {
+    e.preventDefault();
+    let url = "https://cuntato.herokuapp.com/api/project-data";
+    let data = {
+      name: $("#name").val(),
+      email: $("#email").val(),
+      message: $("#message").val(),
+    };
+
+    data = JSON.stringify(data);
+    let projectID = "7tbe097af6";
+    let currentUrl = window.location.href;
+    $.post(
+      url,
+      { data, projectID: projectID, currentUrl: currentUrl },
+      function () {}
+    )
+      .done(() => {
+        M.toast({ html: "Message sent 🤗" });
+      })
+      .fail(() => {
+        M.toast({ html: "Something went wrong 😟" });
+      });
+  });
 });
